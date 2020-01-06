96a4a256a2d429ca633d7fbb1159a0e5b63a65b5
gaoweijun <gaoweijun@tellhow.com>
2020-01-06 14:11:37 +0800

    add eslint rule semi 2 always

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ app.use(async (ctx, next) => {
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello world!</h1>';
     console.log(`请求地址：${ctx.path}, 相应时间: ${et - st}`);
-})
+});
 
 app.use(async (ctx, next) => {
     console.log('中间件1，do something');
     await next();
     console.log('中间件1 end');
-})
+});
 
 app.use(async (ctx, next) => {
     console.log('中间件2，do something');
     await next();
     console.log('中间件2 end');
-})
+});
 
 app.use(async (ctx, next) => {
     console.log('中间件3，do something');
     await next();
     console.log('中间件3 end');
-})
+});
 
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
-})
+});
